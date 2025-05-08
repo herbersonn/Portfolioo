c32b365b66dadc58213ecb70edddf2719aee3e51
Herberson Vinicius Sousa Falcão <sousaherberson@gmail.com>
2025-05-08 14:06:50 -0300

    portfo
    vini tarde esc

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,78 +1,62 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import ProjectCard from "./ProjectCard";
-import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
 
 const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
-    image: "/images/projects/1.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    title: "Atena Calc ",
+    description: "Site de cálculos",
+    image: "/images/projects/AtenaCalc.png",
+    gitUrl: "https://github.com/herbersonn/AtenaCalc",
+    previewUrl: "https://nataxpf.github.io/atena-calc/",
   },
   {
     id: 2,
-    title: "Potography Portfolio Website",
-    description: "Project 2 description",
-    image: "/images/projects/2.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    title: "Site do Anime Haikyu",
+    description: "Informações e habilidades dos personagens",
+    image: "/images/projects/SiteHaikyu.png",
+    gitUrl: "https://github.com/herbersonn/Trabalho-final-2023",
+    previewUrl: "https://pedrolima07.github.io/trabalho-html/",
   },
   {
     id: 3,
-    title: "E-commerce Application",
-    description: "Project 3 description",
-    image: "/images/projects/3.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    title: "Índice de Massa Corporal",
+    description: "Site para Calcular IMC",
+    image: "/images/projects/imc.png",
+    gitUrl: "https://github.com/herbersonn/Site-responsivo-de-IMC",
+    previewUrl: "https://renandev20.github.io/IMC-Enfermagem/",
   },
   {
     id: 4,
-    title: "Food Ordering Application",
-    description: "Project 4 description",
+    title: "AtenaVest",
+    description: "Prototipo de APP Mobile de Estudos para Vestibulares",
     image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://www.figma.com/proto/IFeHFXXGvSoggwIfR6Aku7/trab-final?node-id=18-58&p=f&t=LIUPgT5wiTrKxhA1-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A3",
+    previewUrl: "https://www.figma.com/proto/IFeHFXXGvSoggwIfR6Aku7/trab-final?node-id=18-58&p=f&t=LIUPgT5wiTrKxhA1-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A3",
   },
   {
     id: 5,
-    title: "React Firebase Template",
-    description: "Authentication and CRUD operations",
+    title: "RESP",
+    description: "Robotic Emotional Support Pillow",
     image: "/images/projects/5.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/herbersonn/R.E.S.P",
+    previewUrl: "https://github.com/herbersonn/R.E.S.P",
   },
   {
     id: 6,
     title: "Full-stack Roadmap",
     description: "Project 5 description",
     image: "/images/projects/6.png",
-    tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
-  const [tag, setTag] = useState("All");
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
-
-  const handleTagChange = (newTag) => {
-    setTag(newTag);
-  };
-
-  const filteredProjects = projectsData.filter((project) =>
-    project.tag.includes(tag)
-  );
 
   const cardVariants = {
     initial: { y: 50, opacity: 0 },
@@ -82,36 +66,18 @@ const ProjectsSection = () => {
   return (
     <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
+        Meus Projetos
       </h2>
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <ProjectTag
-          onClick={handleTagChange}
-          name="All"
-          isSelected={tag === "All"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="Web"
-          isSelected={tag === "Web"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="Mobile"
-          isSelected={tag === "Mobile"}
-        />
-      </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {filteredProjects.map((project, index) => (
+        {projectsData.map((project, index) => (
           <motion.li
-            key={index}
+            key={project.id}
             variants={cardVariants}
             initial="initial"
             animate={isInView ? "animate" : "initial"}
             transition={{ duration: 0.3, delay: index * 0.4 }}
           >
             <ProjectCard
-              key={project.id}
               title={project.title}
               description={project.description}
               imgUrl={project.image}
